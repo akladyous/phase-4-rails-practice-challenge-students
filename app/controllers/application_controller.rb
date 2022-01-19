3e2af7c274f8b7ccb61eb2d7093574de96c2d5a1
Boula Akladyous <akladyous@gmail.com>
2022-01-19 17:42:38 -0500

    completed part.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::API
     rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
     rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+    # rescue_from ActiveController::RoutingError, with: :invalid_routes
 
+    before_action :vertify_ip_address
+
+    def invalid_routes
+        # byebug
+        render json: {error: "error"}
+        # raise ActionController::RoutingError.new("No route matches #{params[:unmatched_route]}")
+    end
     private
     def record_invalid(exception_msg)
         render json: {error: exception_msg.record.errors.full_messages}, status: :unprocessable_entity
@@ -10,5 +18,9 @@ class ApplicationController < ActionController::API
     def record_not_found(exception_msg)
         # byebug
         render json: {error: exception_msg.message}, status: :not_found
+    end
+
+    def vertify_ip_address
+        # byebug
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :instructors, only: [:create, :show, :update, :destroy] do
     resources :students, only: [:create, :update]
   end
+  get '*unmatched_route', :to => 'application#invalid_routes'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
